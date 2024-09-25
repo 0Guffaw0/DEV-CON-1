1c8a3dbed6dd6d5cb0298b4d31c1914467598320
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-09-25 16:26:31 -0400

    Add scripts + mechanics

---
+++ RapidPrototype1/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+    private bool isInBox = false;
+
+    public float runSpeed = 10f;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Movement(h);
+        HandleGravity();
+    }
+
+    void Movement(float hSpeed)
+    {
+        float xVel = hSpeed * runSpeed;
+        rb2d.velocity = new Vector2(xVel, rb2d.velocity.y);
+
+        // Flip sprite based on direction
+        if (hSpeed < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (hSpeed > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+
+    void HandleGravity()
+    {
+        if (isInBox)
+        {
+            rb2d.gravityScale = 1;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.gravityScale = -1;
+        }
+        else
+        {
+            rb2d.gravityScale = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Soot"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("gravityBox"))
+        {
+            isInBox = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("gravityBox"))
+        {
+            isInBox = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "endDoor")
+        {
+            SceneManager.LoadScene("theEnd");
+        }
+    }
+}
---
+++ RapidPrototype1/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    private Transform target;
+    public Vector3 offset = Vector3.zero;
+    public float smoothing = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos + offset, smoothing * Time.deltaTime);
+    }
+}
