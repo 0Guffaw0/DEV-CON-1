5c57aca3d2433adff9efabfb56b568fcd2e35221
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-09-25 18:41:41 -0400

    Add lift timer + UI

--- RapidPrototype1/Assets/Scripts/Player.cs
+++ RapidPrototype1/Assets/Scripts/Player.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private SpriteRenderer sr;
     private bool isInBox = false;
+    private float spaceHoldTime = 0f;
+    public float maxHoldTime = 3f;
 
     public float runSpeed = 10f;
     public LayerMask groundLayer;
+    public TextMeshProUGUI timerText;
 
     void Start()
     {
@@ -24,6 +28,7 @@ public class Player : MonoBehaviour
 
         Movement(h);
         HandleGravity();
+        UpdateTimerUI();
     }
 
     void Movement(float hSpeed)
@@ -46,15 +51,32 @@ public class Player : MonoBehaviour
     {
         if (isInBox)
         {
-            rb2d.gravityScale = 1;
+            rb2d.gravityScale = 5;
         }
-        else if (Input.GetKey(KeyCode.Space))
+        else if (Input.GetKey(KeyCode.Space) && spaceHoldTime < maxHoldTime)
         {
             rb2d.gravityScale = -1;
+            spaceHoldTime += Time.deltaTime;
         }
         else
         {
             rb2d.gravityScale = 5;
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                spaceHoldTime = 0f;
+            }
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            timerText.text = "LiftTime: " + (maxHoldTime - spaceHoldTime).ToString("F2") + "s";
+        }
+        else
+        {
+            timerText.text = "LiftTime: " + maxHoldTime.ToString("F2") + "s";
         }
     }
 
